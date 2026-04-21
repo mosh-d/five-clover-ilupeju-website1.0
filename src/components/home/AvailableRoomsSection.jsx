dd4aec73a75f4b8969eb0940b5e6f84f7223a665
mosh-d <shopsy8@gmail.com>
2026-04-21 17:51:04 +0100

    Fixing popup bug and correcting spelling in footer

--- src/components/home/AvailableRoomsSection.jsx
+++ src/components/home/AvailableRoomsSection.jsx
@@ -210,12 +210,8 @@ export default function AvailableRoomsSection() {
     "Executive Deluxe": mobileExecutiveDeluxeImage,
   };
 
-  const roomGalleryImages = isMobile
-    ? mobileRoomGalleryImages
-    : desktopRoomGalleryImages;
-  const roomPrimaryImages = isMobile
-    ? mobileRoomTypeImages
-    : desktopRoomTypeImages;
+  const roomGalleryImages = desktopRoomGalleryImages;
+  const roomPrimaryImages = desktopRoomTypeImages;
 
   const handleViewImages = (images) => {
     if (!images || images.length === 0) return;
